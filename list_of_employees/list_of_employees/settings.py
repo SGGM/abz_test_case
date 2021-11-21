832bcd8162df6194ef79be581d08fbd91d823bcc
SGGM <gleb_somov@mail.ru>
2021-11-21 13:24:08 +0300

    managed security leaks, kinda ;) Upd requirements

--- list_of_employees/list_of_employees/settings.py
+++ list_of_employees/list_of_employees/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a0%08p$tb3*oq3smkxjss2@k(n%96i^pzkod$dzinvlcv9!^y_'
+SECRET_KEY = os.environ.get('ABZ_TEST_EXERCISE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'list_of_employees.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('ABZ_TEST_EXERCISE_DB_POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
