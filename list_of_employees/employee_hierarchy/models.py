6f01e9753de180c43d92304f8b9dcacfa81b4b6a
SGGM <gleb_somov@mail.ru>
2021-11-25 16:54:48 +0300

    forgot to ctrl+s half of the Employee model. Already made migrations and migrated...

--- list_of_employees/employee_hierarchy/models.py
+++ list_of_employees/employee_hierarchy/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.db.models import manager
 from django.db.models.deletion import CASCADE
 from mptt.models import MPTTModel, TreeForeignKey
 
 
 
 class Employee(MPTTModel):
-    parent = TreeForeignKey('self', on_delete=CASCADE, null=True, blank=True,
+    manager = TreeForeignKey('self', on_delete=CASCADE, null=True, blank=True,
                              related_name='children')
     full_name = models.CharField(max_length=80)
+    position = models.CharField(max_length=50)
+    employment_date = models.DateField()
+    salary = models.IntegerField()
