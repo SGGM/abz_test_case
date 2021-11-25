979cd337125c3c423552d741ac2f2418d33bb670
SGGM <gleb_somov@mail.ru>
2021-11-25 16:43:46 +0300

    made an Employee model. It turned out, you can make recursive structures without mptt, django allows to do it. What if the difference than...will find out

--- list_of_employees/employee_hierarchy/models.py
+++ list_of_employees/employee_hierarchy/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+
+class Employee(MPTTModel):
+    parent = TreeForeignKey('self', on_delete=CASCADE, null=True, blank=True,
+                             related_name='children')
+    full_name = models.CharField(max_length=80)
--- list_of_employees/list_of_employees/settings.py
+++ list_of_employees/list_of_employees/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'employee_hierarchy',
 
     # Third-party apps
-    'mttp',
+    'mptt',
 ]
 
 MIDDLEWARE = [
